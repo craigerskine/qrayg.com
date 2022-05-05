cc7b7ab648f5c417fcae05250f3f142cfcb1fb5d
Craig - qrayg <craig@craigerskine.com>
2022-05-05 15:54:24 -0500

    ...

--- pages/error.vue.js
+++ pages/error.vue.js
@@ -1,6 +1,6 @@
 const _error = {
 	template: `
-    <section class="w-full h-screen flex">
+    <section class="w-full h-screen flex transition-all">
       <article class="m-auto">
         <router-link to="/" class="fixed top-3 left-3">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" role="img" class="w-8 h-auto">
