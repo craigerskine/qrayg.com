5bcb8116d3d34a8e14b35cbd13d7a74257c0d996
Craig - qrayg <craig@craigerskine.com>
2022-08-28 11:48:54 -0500

    ...

--- pages/gear.vue.js
+++ pages/gear.vue.js
@@ -109,7 +109,7 @@ const Gear = {
             { type: 'KB', name: 'Leopold › FC660C', sub: ['65%', 'White', 'Topre Silent', 'Extended2048'], link: 'https://amzn.com/dp/B07QKXC6WC?tag=qrayg-20', },
             { type: 'KB', name: 'Morgrie', sub: ['65%', 'Clicky'], link: 'https://morgrie.com/', },
             { type: 'KB', name: 'Candybar', sub: ['50%', 'Black', 'Summer', 'Outemu Tactile'], link: 'https://thekey.company/collections/candybar-round-3/', },
-            { soon: true, type: 'KB', name: 'Keychron › Q9', sub: ['45%', 'Silver', 'Knob', 'Silent Alpaca'], link: 'https://keychron.com/products/keychron-q9-qmk-custom-mechanical-keyboard', },
+            { type: 'KB', name: 'Keychron › Q9', sub: ['45%', 'Silver', 'Knob', 'Silent Alpaca'], link: 'https://keychron.com/products/keychron-q9-qmk-custom-mechanical-keyboard', },
             { type: 'KB', name: 'Cajal', sub: ['45%', 'Space Grey', 'Silent Sky'], link: 'https://walletburner.co/pages/cajal', },
             { type: 'KB', name: 'UT47.2', sub: ['40%', 'Alu', 'Kailh Copper'], link: 'https://keyhive.xyz/shop/ut472-kit', },
             { type: 'KB', name: 'Planck EZ › Glow x2', sub: ['40%', 'Black - Kaihl Gold', 'White - Zilent'], link: 'https://ergodox-ez.com/pages/planck', },
@@ -123,8 +123,8 @@ const Gear = {
             { type: 'Caps', name: 'Plastic', sub: ['Clone', 'XDA'], link: 'https://amzn.com/dp/B08ZMR1651?tag=qrayg-20', },
             { type: 'Caps', name: 'GMK Dolch 2', sub: ['Modern', 'Standard', 'Obscure'], link: 'https://omnitype.com/', },
             { type: 'Caps', name: 'DSA Milkshake', sub: ['Weirdo', 'Spacebars', 'Fruit'], link: 'https://novelkeys.com/collections/keycaps/products/dsa-milkshake', },
-            { soon: true, type: 'Caps', name: 'Domikey Ghost', sub: ['Extension'], link: 'https://cannonkeys.com/collections/group-buy/products/gb-domikey-ghost', },
             { soon: true, type: 'Caps', name: 'Less But Better', sub: ['Novelties', 'Extras'], link: 'https://kono.store/products/epbt-less-but-better', },
+            { soon: true, type: 'Caps', name: 'Domikey Ghost', sub: ['Extension'], link: 'https://cannonkeys.com/collections/group-buy/products/gb-domikey-ghost', },
             { soon: true, type: 'Caps', name: 'kam_superuser', sub: ['Mods Highlight', '40\'s Highlight', 'Spacebars', 'Novelties'], link: 'https://novelkeys.com/products/kam-superuser', },
           ]
         },
