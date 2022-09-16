dcc11fc1f790311cf5db8445f7aad7f1fb5a1dde
Craig - qrayg <craig@craigerskine.com>
2022-09-16 15:07:54 -0500

    ...

--- pages/gear.vue.js
+++ pages/gear.vue.js
@@ -134,7 +134,8 @@ const Gear = {
           color: 'purple-500',
           hardware: [
             { type: 'Capture', name: 'Elgato › HD60 S', sub: ['USB'], link: 'https://amzn.com/dp/B01DRWCOGA?tag=qrayg-20', },
-            { type: 'Cam', name: 'Lumina › AI Webcam', sub: ['USB', 'Grey'], link: 'https://getlumina.com/', },
+            //{ type: 'Cam', name: 'Lumina › AI Webcam', sub: ['USB', 'Grey'], link: 'https://getlumina.com/', },
+            { type: 'Cam', name: 'Opal › AI Webcam', sub: ['USB', 'Black'], link: 'https://opalcamera.com/', },
             { type: 'Mic', name: 'Shure › MV7', sub: ['USB', 'XLR'], link: 'https://amzn.com/dp/B08G7RG9ML?tag=qrayg-20', },
             { type: 'Strip HDCP', name: 'OREI › HD-102', sub: ['HDMI'], link: 'https://amzn.com/dp/B005HXFARS?tag=qrayg-20', },
             { type: 'Misc', name: 'LaMetric › Time', sub: ['Smart Clock'], link: 'https://amzn.com/dp/B01IVTVK3W?tag=qrayg-20', },
