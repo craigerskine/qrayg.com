be4ee9ebb6978bf05349a2aead0c0e677e29cc00
Craig - qrayg <craig@craigerskine.com>
2022-10-14 15:39:47 -0500

    ...

--- pages/gear.vue.js
+++ pages/gear.vue.js
@@ -165,11 +165,11 @@ const Gear = {
             { type: 'Guitar', name: '.strandberg* › Boden', sub: ['2020', 'Metal 6', 'Black Pearl'], link: 'https://strandbergguitars.com/product-category/family/boden-metal/', },
             { type: 'Amp', name: 'Positive Grid › Spark 40', sub: ['40 watt'], link: 'https://amzn.com/dp/B08HQCG79H?tag=qrayg-20', },
             { type: 'Amp', name: 'Positive Grid › Spark MINI', sub: ['10 watt'], link: 'https://www.positivegrid.com/spark-mini', },
-            { type: 'Amp', name: 'Boss › Katana MINI', sub: ['7 watt'], link: 'https://amzn.com/dp/B072XKP8TH?tag=qrayg-20', },
-            { type: 'Modeling', name: 'Line 6 › HX Stomp', sub: ['USB'], link: 'https://amzn.com/dp/B07J2LV1N1?tag=qrayg-20', },
+            //{ type: 'Amp', name: 'Boss › Katana MINI', sub: ['7 watt'], link: 'https://amzn.com/dp/B072XKP8TH?tag=qrayg-20', },
+            //{ type: 'Modeling', name: 'Line 6 › HX Stomp', sub: ['USB'], link: 'https://amzn.com/dp/B07J2LV1N1?tag=qrayg-20', },
             { type: 'Plugins', name: 'Neural DSP › Archetype', sub: ['Petrucci', 'Tim Henson'], link: 'https://neuraldsp.com/plugins', },
             { type: 'Sequencer', name: 'Arturia › Keystep', sub: ['MIDI Controller'], link: 'https://amzn.com/dp/B01BPSBU40?tag=qrayg-20', },
-            { type: 'Interface', name: 'AXE › I/O Solo', sub: ['Bus Powered'], link: 'https://amzn.com/dp/B085JDFLLD?tag=qrayg-20', },
+            { type: 'Interface', name: 'AXE › I/O Solo', sub: ['+ Amplitube'], link: 'https://amzn.com/dp/B085JDFLLD?tag=qrayg-20', },
           ]
         },
       ]
