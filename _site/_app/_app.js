e1817901aaa4a5173e99464b26a4d7d87916ded3
Craig @craigerskine @qrayg <craig@craigerskine.com>
2025-03-27 14:06:38 -0500

    removed qnx

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -16,19 +16,11 @@ install({
     extend: {
       colors: ({ theme }) => ({
         gray: theme('colors.zinc'),
-        'qnx-cream': '#E7E3D6',
-        'qnx-theme': '#98A8C8',
-        'qnx-lite': '#F0F0F0',
-        'qnx-base': '#D8D8D8',
-        'qnx-item': '#C8CCC8',
-        'qnx-dark': '#B8BCB8',
-        'qnx-line': '#484848',
       }),
       fontFamily: ({ theme }) => ({
         sans: ['Inter', ...theme('fontFamily.sans')],
         mono: ['Inconsolata', ...theme('fontFamily.mono')],
         roboto: ['Roboto Mono', ...theme('fontFamily.mono')],
-        tahoma: ['Tahoma', ...theme('fontFamily.sans')],
       }),
       boxShadow: {
         'up': '0 -5px 15px 0 rgba(0,0,0,.2)',
@@ -66,8 +58,6 @@ install({
   },
   rules: [
     [ 'bg-grid', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 4 4\' width=\'4\' height=\'4\'><rect x=\'0\' y=\'0\' width=\'2\' height=\'2\' fill=\'rgba(5,5,5,.33)\'></rect></svg>")', } ],
-    [ 'qnx-bevel', { '@apply': 'bg-qnx-base border border-t-qnx-lite border-l-qnx-lite border-b-qnx-dark border-r-qnx-dark flex-1 flex relative' } ],
-    [ 'qnx-bevel-sunk', { '@apply': 'bg-qnx-base border border-t-qnx-dark border-l-qnx-dark border-b-qnx-light border-r-qnx-light flex-1 flex relative' } ],
   ],
 });
 injectGlobal`
@@ -77,8 +67,6 @@ injectGlobal`
     .social a iconify-icon { @apply text-white opacity-50 relative z-20 motion-safe:transition-all; }
     .social .active,.social a:hover,.social a:focus { @apply motion-safe:animate-blob; }
     .social .active iconify-icon,.social a:hover iconify-icon,.social a:focus iconify-icon { @apply text-white opacity-100; }
-    .qnx-bg-title { @apply bg-gradient-to-b from-[#F7F7F7] to-[#9C9E9C]; }
-    .qnx-bg-title-active { @apply bg-gradient-to-b from-[#638ECE] to-[#396194]; }
   }
 `
 
